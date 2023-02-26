d7c60aa3e7f407265db428dbee96fd0dca4507c4
Adlet <adlet.zh19966@gmail.com>
2023-02-26 12:35:42 +0600

    add startBt/restartBt/buttonsSV
    startButton/restartButton/buttonsStackView

--- Pomodoro timer app/Sources/App/ViewController.swift
+++ Pomodoro timer app/Sources/App/ViewController.swift
@@ -17,13 +17,36 @@ class ViewController: UIViewController {
         appLabel.font = UIFont(name: "Marker Felt", size: 30)
         return appLabel
     }()
-    
     private lazy var timerLabel: UILabel = {
        let timerLabel = UILabel()
         timerLabel.text = "25:00"
         timerLabel.textColor = .black
         timerLabel.font = UIFont(name: "Times New Roman", size: 30)
         return timerLabel
+    }()
+    private lazy var restartButton: UIButton = {
+       let restartButton = UIButton()
+        restartButton.setTitle("Restart", for: .normal)
+        restartButton.setTitleColor(.black, for: .normal)
+        restartButton.titleLabel?.font = UIFont(name: "Times New Roman", size: 30)
+        restartButton.addTarget(self, action: #selector(restartButtonTapped), for: .touchUpInside)
+        return restartButton
+    }()
+    private lazy var startButton: UIButton = {
+       let startButton = UIButton()
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(.black, for: .normal)
+        startButton.titleLabel?.font = UIFont(name: "Times New Roman", size: 30)
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        return startButton
+    }()
+    private lazy var buttonsSV: UIStackView = {
+       let buttonsSV = UIStackView()
+        buttonsSV.axis = .horizontal
+        buttonsSV.spacing = 170
+        buttonsSV.addArrangedSubview(restartButton)
+        buttonsSV.addArrangedSubview(startButton)
+        return buttonsSV
     }()
 
     override func viewDidLoad() {
@@ -34,8 +57,15 @@ class ViewController: UIViewController {
         setupLayout()
     }
     
+    @objc private func startButtonTapped() {
+        
+    }
+    @objc private func restartButtonTapped() {
+        
+    }
+    
     private func setupHierarchy() {
-        let views = [timerLabel,appLabel]
+        let views = [timerLabel,appLabel,buttonsSV]
         views.forEach { view.addSubview($0) }
     }
     private func setupLayout() {
@@ -47,6 +77,11 @@ class ViewController: UIViewController {
             appLabel.centerX.equalTo(view)
             appLabel.top.equalTo(view).offset(100)
         }
+        buttonsSV.snp.makeConstraints { buttonsSV in
+            buttonsSV.centerX.equalTo(view)
+            buttonsSV.bottom.equalTo(view).inset(75)
+        }
+        
     }
 
 
